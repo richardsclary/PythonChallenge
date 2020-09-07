046d1bb2543cd00ed2e689c1320a8282fc834cc5
Rick Clary <68802369+richardsclary@users.noreply.github.com>
2020-09-07 03:59:15 -0400

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,7 +27,7 @@ headers = []
 #rows = []
 #filepath = '~/Users/rsc/Desktop/BCS Homework Assignments/HW #3/PythonChallenge/PyBank/Resources/
 # opening the CSV file 
-with open('budget_data.csv', mode ='r') as file: 
+with open('Resources/budget_data.csv', mode ='r') as file: 
     
 # reading the CSV file 
     csvFile = csv.reader(file) 
