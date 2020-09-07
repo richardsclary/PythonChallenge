5e06e2f51216168be5c3a1a9ff62645cc39b0ca6
Rick Clary <68802369+richardsclary@users.noreply.github.com>
2020-09-06 23:41:23 -0400

    Title and Notes Added to File

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+#####------------------------------
+##### This python script was created to analyze the financial
+##### data of a company located in the file, "budget_data.csv"
+##### located in the Resources folder, both of which are located 
+##### under the home directory, 'PyBank'.
+##### 
+
+
